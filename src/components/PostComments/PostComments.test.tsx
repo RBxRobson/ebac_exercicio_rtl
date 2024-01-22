b71323b8788c9f05a64a73fa73b86712487168dc
Robson.B <robsonnbarbosa0@gmail.com>
2024-01-22 14:33:25 -0300

    add - testes para adição de comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
+//* Função que responsável por renderizar o componente e adicionar um comentário
+const renderComments = (comment: string) => {
+    render(<PostComment />);
+    const textarea = screen.getByTestId('textarea');
+    const btn = screen.getByTestId('post-btn');
+    
+    fireEvent.change(textarea, { target: { value: comment } });
+    fireEvent.click(btn);
+};
+
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    //* teste de renderização do comentário 1
+    test('Deve adicionar o comentário "Muito bom"', () => {
+        renderComments('Muito bom')
+        expect(screen.getByText('Muito bom')).toBeInTheDocument();
+    });
+
+    //* teste de renderização do comentário 2
+    test('Deve adicionar o comentário "Gostei do projeto"', () => {
+        renderComments('Gostei do projeto')
+        expect(screen.getByText('Gostei do projeto')).toBeInTheDocument();
     });
 });
